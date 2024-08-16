b043fa14e77ac561da2a3f26d5399d61841a74a7
karen <quemojojojo@gmail.com>
2024-08-15 21:13:06 -0500

    feat: styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Form from "./components/form/Form";
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Tipos de animales</h1>
-      <Form />
+    <div className={styles.mainContainer}>
+      <div className={styles.innerContainer}>
+        <div className={styles.imgbox}>
+        </div>
+        <div className={styles.box}>
+          <h1>Tipos de animales</h1>
+          <Form />
+        </div>
+      </div>
     </div>
   );
 }
